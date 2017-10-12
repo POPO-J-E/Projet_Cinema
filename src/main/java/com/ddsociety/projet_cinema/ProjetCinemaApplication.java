0eb0c927959a3afb4325dad2d6c8de685a27f11a
Etienne DEBARD <etienne.debard@etu.univ-lyon1.fr>
2017-10-12 08:30:00 +0200

    init project

---
+++ src/main/java/com/ddsociety/projet_cinema/ProjetCinemaApplication.java
@@ -0,0 +1,12 @@
+package com.ddsociety.projet_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCinemaApplication.class, args);
+	}
+}
